a9112baf858e8aa6eeb2627b84d94ee6c2fe084f
ShivdasJadhav <jshivdas07@gmail.com>
2023-04-18 09:45:20 +0530

    first commit to searchEngine to add files

---
+++ home.js
@@ -0,0 +1,42 @@
+let content = document.getElementById("content");
+let dev = document.getElementById("developer");
+function home() {
+  const replace_content = new XMLHttpRequest();
+  replace_content.open("get", "google_search.html", true);
+  replace_content.onload = () => {
+    if (replace_content.status === 200) {
+      let newcontent = replace_content.response;
+      content.innerHTML = `${newcontent}`;
+    } else {
+      alert("some error occured to load Google Search!");
+    }
+  };
+  replace_content.send();
+}
+home();
+function image_search() {
+  const replace_content = new XMLHttpRequest();
+  replace_content.open("get", "image_search.html", true);
+  replace_content.onload = () => {
+    if (replace_content.status === 200) {
+      let newcontent = replace_content.response;
+      content.innerHTML = `${newcontent}`;
+    } else {
+      alert("some erroe occured to load image Search!");
+    }
+  };
+  replace_content.send();
+}
+function advance_search() {
+  const replace_content = new XMLHttpRequest();
+  replace_content.open("get", "advance_search.html", true);
+  replace_content.onload = () => {
+    if (replace_content.status === 200) {
+      let newcontent = replace_content.response;
+      content.innerHTML = `${newcontent}`;
+    } else {
+      alert("some erroe occured to load advance Search!");
+    }
+  };
+  replace_content.send();
+}
